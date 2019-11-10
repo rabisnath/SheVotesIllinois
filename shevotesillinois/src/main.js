17330c581475a8a5b36b64c96f6b55bc82b48b7c
Adela Deng <dengyijia@cs.uchicago.edu>
2019-11-10 12:19:13 -0600

    add server

--- shevotesillinois/src/main.js
+++ shevotesillinois/src/main.js
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import Home from './components/Home.vue';
+
 
 Vue.config.productionTip = false;
 
+Vue.use(BootstrapVue);
+Vue.component('home', Home);
+
+Vue.component('home', Home);
 new Vue({
   router,
   render: h => h(App),
