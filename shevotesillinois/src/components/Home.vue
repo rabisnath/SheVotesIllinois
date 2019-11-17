6117c4ec722dd095ade850c87d979d399ea4ca4e
Adela Deng <dengyijia@cs.uchicago.edu>
2019-11-17 11:47:22 -0600

    change indentation

--- shevotesillinois/src/components/Home.vue
+++ shevotesillinois/src/components/Home.vue
@@ -1,69 +1,68 @@
 <template>
-    <b-col>
-        <img src="./title_image.jpg"/>
-        <b-jumbotron>
-    <template v-slot:header>Who we are</template>
+  <b-col>
+    <img src="./title_image.jpg"/>
+    <b-jumbotron>
+      <template v-slot:header>Who we are</template>
 
-    <template v-slot:lead>
-      She Votes Illinois is a statewide political action committee aimed at
-         highlighting the importance of women’s voices in elections, government, and
-          policymaking.
-    </template>
+      <template v-slot:lead>
+        She Votes Illinois is a statewide political action committee aimed at
+           highlighting the importance of women’s voices in elections, government, and
+            policymaking.
+      </template>
 
-    <b-button variant="primary" href="#">Meet our members!</b-button>
-  </b-jumbotron>
-  <b-jumbotron>
-    <template v-slot:header>What we do</template>
+      <b-button variant="primary" href="#">Meet our members!</b-button>
+    </b-jumbotron>
 
-    <template v-slot:lead>
-      Our work focuses on three things: voting, education,
-      and getting more women involved in politics. We are a grassroots team of
-      Illinois women working to engage a new wave of voters, community members,
-      activists, and politicians.
-    </template>
+    <b-jumbotron>
+      <template v-slot:header>What we do</template>
 
-    <hr class="my-4">
-    <b-row center>
-            <b-col>
-                <b-card
-                    title="Voting"
-                    tag="article"
-                    style="max-width: 20rem;"
-                    class="mb-2"
-                  >
-                    <b-card-text>
-                        Make sure all women can vote in every state and local election
-                    </b-card-text>
+      <template v-slot:lead>
+        Our work focuses on three things: voting, education,
+        and getting more women involved in politics. We are a grassroots team of
+        Illinois women working to engage a new wave of voters, community members,
+        activists, and politicians.
+      </template>
 
-                    <b-button
-                    squared variant="outline-secondary"
-                    href="#">Become Aware</b-button>
-                  </b-card>
-            </b-col>
-            <b-col>
-              <b-card
-                title="Education"
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-              >
-                <b-card-text>
-                  Ensure our elected officials and candidates for office understand
-                  the issues most important to Illinois women
-                </b-card-text>
+      <hr class="my-4">
 
-                <b-button
-                    squared variant="outline-secondary"
-                    href="#">Be Informed</b-button>
-              </b-card>
-            </b-col>
-            <b-col>
+      <b-row center>
+        <b-col>
+          <b-card
+              title="Voting"
+              tag="article"
+              style="max-width: 20rem;"
+              class="mb-2">
+              <b-card-text>
+                  Make sure all women can vote in every state and local election
+              </b-card-text>
+
+              <b-button
+              squared variant="outline-secondary"
+              href="#">Become Aware</b-button>
+            </b-card>
+        </b-col>
+        <b-col>
+          <b-card
+            title="Education"
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2">
+            <b-card-text>
+              Ensure our elected officials and candidates for office understand
+              the issues most important to Illinois women
+            </b-card-text>
+
+            <b-button
+                squared variant="outline-secondary"
+                href="#">Be Informed</b-button>
+          </b-card>
+        </b-col>
+        <b-col>
           <b-card
             title="Engagement"
             tag="article"
             style="max-width: 20rem;"
-            class="mb-2"
-          >
+            class="mb-2">
             <b-card-text>
               Support women entering into state and local politic
             </b-card-text>
@@ -71,29 +70,28 @@
                     squared variant="outline-secondary"
                     href="#">Get Active</b-button>
           </b-card>
-      </b-col>
+        </b-col>
       </b-row>
+    </b-jumbotron>
+    <b-jumbotron>
+      <template v-slot:header>History</template>
 
-  </b-jumbotron>
-        <b-jumbotron>
-    <template v-slot:header>History</template>
+      <template v-slot:lead>
+        She Votes Illinois was founded when a few women got together over coffee in 2017.
+        We were disappointed that there were no women running for governor of
+        Illinois—especially since Illinois has never had a woman serve as governor.
+      </template>
 
-    <template v-slot:lead>
-      She Votes Illinois was founded when a few women got together over coffee in 2017.
-      We were disappointed that there were no women running for governor of
-      Illinois—especially since Illinois has never had a woman serve as governor.
-    </template>
+      <hr class="my-4">
 
-    <hr class="my-4">
+      <p>
+        More than 500 people attended our inaugural event, where we questioned and
+         challenged the candidates on the record on a wide range of issues that
+         included violence against women, equal pay for equal work, and access
+         to choice in healthcare.
+      </p>
 
-    <p>
-      More than 500 people attended our inaugural event, where we questioned and
-       challenged the candidates on the record on a wide range of issues that
-       included violence against women, equal pay for equal work, and access
-       to choice in healthcare.
-    </p>
-
-    <b-button variant="primary" href="#">More history</b-button>
-  </b-jumbotron>
-    </b-col>
+      <b-button variant="primary" href="#">More history</b-button>
+    </b-jumbotron>
+  </b-col>
 </template>
